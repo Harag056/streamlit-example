505f6bde658e74db3f96b1905b19a22956ab2263
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-20 23:03:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,24 @@ snowflake_schema = st.text_input("Schema", "")
 
 if st.button("Connect"):
     # Connect to Snowflake
-    try:
-        connection = snowflake.connector.connect(
-            account=snowflake_account,
-            user=snowflake_user,
-            password=snowflake_password,
-            warehouse=snowflake_warehouse,
-            database=snowflake_database,
-            schema=snowflake_schema
-        )
-        cursor = connection.cursor()
+    #try:
+    connection = snowflake.connector.connect(
+        account=snowflake_account,
+        user=snowflake_user,
+        password=snowflake_password,
+        warehouse=snowflake_warehouse,
+        database=snowflake_database,
+        schema=snowflake_schema
+    )
+    cursor = connection.cursor()
 
-        # Execute a sample query
-        cursor.execute("SELECT CURRENT_TIMESTAMP() as CurrentTime;")
-        result = cursor.fetchone()
+    # Execute a sample query
+    cursor.execute("SELECT CURRENT_TIMESTAMP() as CurrentTime;")
+    result = cursor.fetchone()
 
-        st.success(f"Connected to Snowflake! Current timestamp: {result[0]}")
-        connection.close()
-    except Exception as e:
-        st.error(f"Error: {e}")
-        connection.close()
+    st.success(f"Connected to Snowflake! Current timestamp: {result[0]}")
+    connection.close()
+   # except Exception as e:
+     #   st.error(f"Error: {e}")
+     #   connection.close()
 
