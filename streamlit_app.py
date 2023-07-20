0d1ed64bf302c3f4da4cfefd916713708777a1f2
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-20 13:26:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import snowflake.connector
 
-"""
-# Welcome to Streamlit!
+def main():
+    st.title("Snowflake Connection App")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Input fields for Snowflake connection parameters
+    snowflake_account = st.text_input("Snowflake Account", "")
+    snowflake_user = st.text_input("User", "")
+    snowflake_password = st.text_input("Password", "", type="password")
+    snowflake_warehouse = st.text_input("Warehouse", "")
+    snowflake_database = st.text_input("Database", "")
+    snowflake_schema = st.text_input("Schema", "")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    if st.button("Connect"):
+        # Connect to Snowflake
+        try:
+            connection = snowflake.connector.connect(
+                account=snowflake_account,
+                user=snowflake_user,
+                password=snowflake_password,
+                warehouse=snowflake_warehouse,
+                database=snowflake_database,
+                schema=snowflake_schema
+            )
+            cursor = connection.cursor()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+            # Execute a sample query
+            cursor.execute("SELECT CURRENT_TIMESTAMP() as CurrentTime;")
+            result = cursor.fetchone()
 
+            st.success(f"Connected to Snowflake! Current timestamp: {result[0]}")
+            connection.close()
+        except Exception as e:
+            st.error(f"Error: {e}")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
