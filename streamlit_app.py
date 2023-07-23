e7de5568fd563c4fa48c072972ac0d1e660b78b3
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-23 23:36:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,22 @@ import streamlit as st
 import requests
 
 LANDING_AI_API_KEY = "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"
-LANDING_AI_ENDPOINT_ID = "5bc96d69-6328-410f-83e2-eb3b5d97ad29"
-LANDING_AI_API_URL = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=456de868-464d-45e3-8f6a-8d9a8e1301ab"  # Replace this with the actual API URL
+LANDING_AI_API_ENDPOINT = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=456de868-464d-45e3-8f6a-8d9a8e1301ab"  # Replace this with the actual API endpoint URL
 
-def upload_image(image_path):
+def landing_ai_api_request(api_endpoint, headers, data):
     try:
-        headers = {
-            "Authorization": f"Bearer {LANDING_AI_API_KEY}",
-        }
-
-        files = {
-            "file": open(image_path, "rb"),
-        }
-
-        data = {
-            "endpoint_id": LANDING_AI_ENDPOINT_ID,
-            # Add any other required parameters based on the API documentation
-        }
-
-        response = requests.post(LANDING_AI_API_URL, headers=headers, files=files, data=data)
-
-        if response.status_code == 200:
-            return response.json()
-        else:
-            st.error(f"API request failed with status code: {response.status_code}")
-            return None
-    except Exception as e:
-        st.error(f"Error occurred during API request: {e}")
+        response = requests.post(api_endpoint, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error during API request: {e}")
         return None
 
-st.title("Landing AI Image Upload")
+st.title("Landing AI API Usage")
 
-# File uploader widget to select an image for upload
+# Your Streamlit app UI to accept user inputs or images for processing with the API
+
+# Example: Upload an image using file_uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
@@ -43,16 +27,23 @@ if uploaded_file is not None:
         f.write(uploaded_file.read())
     st.success("Image uploaded successfully.")
 
-    # Display the uploaded image
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    # Prepare headers and data for the API request
+    headers = {
+        "Authorization": f"Bearer {LANDING_AI_API_KEY}",
+        # Add any other required headers based on the API documentation
+    }
 
-    # Use the API to process the image (if Landing AI API is available)
-    if st.button("Process Image"):
-        result = upload_image(image_path)
-        if result:
-            # Process and display the API response as needed
-            st.write("API Response:")
-            st.write(result)
+    data = {
+        # Add any required data or parameters based on the API documentation
+    }
+
+    # Use the API to process the image
+    api_response = landing_ai_api_request(LANDING_AI_API_ENDPOINT, headers=headers, data=data)
+
+    # Process and display the API response as needed
+    if api_response:
+        st.write("API Response:")
+        st.write(api_response)
 
     # Remove the temporary image file after processing
     os.remove(image_path)
