e8ac7878b3518f3785f4037f350555ec4ea835db
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-24 00:52:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import numpy as np
+from PIL import Image
 
-"""
-# Welcome to Streamlit!
+def convert_bin_to_images(binary_data):
+    try:
+        # Convert binary data to a numpy array
+        np_array = np.frombuffer(binary_data, dtype=np.uint8)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+        # Assuming the data represents images, you can reshape it to a suitable shape (e.g., [num_images, height, width, channels])
+        # Replace 'num_images', 'height', 'width', and 'channels' with the actual values based on your binary data format.
+        # For example, if it's a single image with shape (height, width, channels), you can use np_array.reshape((height, width, channels))
+        images = np_array.reshape([num_images, height, width, channels])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+        # Convert numpy arrays to PIL images
+        pil_images = [Image.fromarray(image) for image in images]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+        return pil_images
+    except Exception as e:
+        st.error(f"Error occurred: {e}")
 
+def main():
+    st.title("Binary File to Images Converter")
+    uploaded_file = st.file_uploader("Upload a binary file", type=["bin"])
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    if uploaded_file is not None:
+        # Read binary data from the uploaded file
+        binary_data = uploaded_file.getvalue()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        # Convert binary data to images
+        images = convert_bin_to_images(binary_data)
 
-    points_per_turn = total_points / num_turns
+        # Display the images
+        for i, image in enumerate(images):
+            st.image(image, caption=f"Image {i+1}", use_column_width=True)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
