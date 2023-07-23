8ea6d82718c8ed8d42f35730f4e187b76c086f5d
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-23 23:26:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,19 @@
 import streamlit as st
+import numpy as np
 import landingai
 
-def upload_images():
-    # Get the images from the user.
-    images = st.file_uploader("Upload images", multiple=True)
+# Generate API key and endpoint ID
+api_key = "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"
+endpoint_id = "5bc96d69-6328-410f-83e2-eb3b5d97ad29"
 
-    # Upload the images to Landing AI.
-    for image in images:
-        landingai.upload_image(image)
+# Create predictor object
+predictor = landingai.Predictor(api_key=api_key, endpoint_id=endpoint_id)
 
-# Create a button to upload the images.
-st.button("Upload Images", on_click=upload_images)
+# Load image
+image = np.asarray(Image.open("1644269774_vehicles.jpg"))
 
-# Run the Streamlit app.
-if __name__ == "__main__":
-    st.run()
+# Make prediction
+prediction = predictor.predict(image)
+
+# Print prediction
+st.write(prediction)
