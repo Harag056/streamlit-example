d816984f60a0fd8a2758f60295f66fd5ce5b35ae
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-23 19:54:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import landingai as la
 
+# Get the API key from Landing AI
+api_key = st.secrets["land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"]
+
 # Create a Landing AI client
-client = la.Client(api_key="land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O")
+client = la.Client(api_key=api_key)
+
+# Create a text field
+text_input = st.text_input("Enter some text")
 
 # Classify the text
-text = "This is a sentence."
-classification = client.classify(text)
-
-# Print the classification results
-print(classification)
+if text_input:
+    classification = client.classify(text_input)
+    st.write(classification)
