50d9b0b027b37de13be3f64c53de147d80c9a2be
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-23 17:39:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,29 @@ import cv2
 
 uploaded_file = st.file_uploader("Upload a video file", type=["mp4"])
 
-if uploaded_file is not None:
-    # Read the video file using OpenCV VideoCapture
-    video_capture = cv2.VideoCapture(uploaded_file)
+# if uploaded_file is not None:
+#     # Read the video file using OpenCV VideoCapture
+#     video_capture = cv2.VideoCapture(uploaded_file)
     
-    # Check if the video is opened successfully
-    if not video_capture.isOpened():
-        st.error("Error: Unable to open the video file.")
-    else:
-        st.success("Video file opened successfully.")
+#     # Check if the video is opened successfully
+#     if not video_capture.isOpened():
+#         st.error("Error: Unable to open the video file.")
+#     else:
+#         st.success("Video file opened successfully.")
 
-        # Read and display each frame of the video
-        while True:
-            ret, frame = video_capture.read()
+#         # Read and display each frame of the video
+#         while True:
+#             ret, frame = video_capture.read()
 
-            if not ret:
-                break
+#             if not ret:
+#                 break
 
-            # Convert the frame from BGR to RGB
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             # Convert the frame from BGR to RGB
+#             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Display the frame using Streamlit
-            st.image(frame_rgb, channels="RGB", use_column_width=True)
+#             # Display the frame using Streamlit
+#             st.image(frame_rgb, channels="RGB", use_column_width=True)
 
-    # Release the video capture object and close the video file
-    video_capture.release()
+#     # Release the video capture object and close the video file
+#     video_capture.release()
+
