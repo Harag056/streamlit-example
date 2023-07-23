e79bf74d87884bb79f20fd69f0117c7620041aab
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-23 23:44:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,78 @@
+# import streamlit as st
+# import requests
+# import os
+
 import streamlit as st
-import requests
-import os
+import numpy as np
+import landingai
 
-LANDING_AI_API_KEY = "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"
-LANDING_AI_API_ENDPOINT = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=456de868-464d-45e3-8f6a-8d9a8e1301ab"  # Replace this with the actual API endpoint URL
+# Generate API key and endpoint ID
+api_key = "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"
+endpoint_id = "456de868-464d-45e3-8f6a-8d9a8e1301ab"
 
-def landing_ai_api_request(api_endpoint, headers, data):
-    try:
-        response = requests.post(api_endpoint, headers=headers, data=data)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error during API request: {e}")
-        return None
+# Create predictor object
+predictor = landingai.Predictor(api_key=api_key, endpoint_id=endpoint_id)
 
-st.title("Landing AI API Usage")
+# Add a title to the app
+st.title("Landing AI Inference")
 
-# Your Streamlit app UI to accept user inputs or images for processing with the API
+# Add a button to upload an image
+st.button("Upload Image")
 
-# Example: Upload an image using file_uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+# If the user uploaded an image, load it into a NumPy array
+if st.button("Upload Image"):
+    image = np.asarray(Image.open("my_image.jpg"))
 
-if uploaded_file is not None:
-    # Save the uploaded image to a temporary location
-    image_path = f"temp_{uploaded_file.name}"
-    with open(image_path, "wb") as f:
-        f.write(uploaded_file.read())
-    st.success("Image uploaded successfully.")
+# Make a prediction
+prediction = predictor.predict(image)
 
-    # Prepare headers and data for the API request
-    headers = {
-        "Authorization": f"Bearer {LANDING_AI_API_KEY}",
-        # Add any other required headers based on the API documentation
-    }
+# Display the prediction
+st.write(prediction)
 
-    data = {
-        # Add any required data or parameters based on the API documentation
-    }
 
-    # Use the API to process the image
-    api_response = landing_ai_api_request(LANDING_AI_API_ENDPOINT, headers=headers, data=data)
+# LANDING_AI_API_KEY = "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O"
+# LANDING_AI_API_ENDPOINT = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=456de868-464d-45e3-8f6a-8d9a8e1301ab"  # Replace this with the actual API endpoint URL
 
-    # Process and display the API response as needed
-    if api_response:
-        st.write("API Response:")
-        st.write(api_response)
+# def landing_ai_api_request(api_endpoint, headers, data):
+#     try:
+#         response = requests.post(api_endpoint, headers=headers, data=data)
+#         response.raise_for_status()
+#         return response.json()
+#     except requests.exceptions.RequestException as e:
+#         st.error(f"Error during API request: {e}")
+#         return None
 
-    # Remove the temporary image file after processing
-    os.remove(image_path)
+# st.title("Landing AI API Usage")
+
+# # Your Streamlit app UI to accept user inputs or images for processing with the API
+
+# # Example: Upload an image using file_uploader
+# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+# if uploaded_file is not None:
+#     # Save the uploaded image to a temporary location
+#     image_path = f"temp_{uploaded_file.name}"
+#     with open(image_path, "wb") as f:
+#         f.write(uploaded_file.read())
+#     st.success("Image uploaded successfully.")
+
+#     # Prepare headers and data for the API request
+#     headers = {
+#         "Authorization": f"Bearer {LANDING_AI_API_KEY}",
+#         # Add any other required headers based on the API documentation
+#     }
+
+#     data = {
+#         # Add any required data or parameters based on the API documentation
+#     }
+
+#     # Use the API to process the image
+#     api_response = landing_ai_api_request(LANDING_AI_API_ENDPOINT, headers=headers, data=data)
+
+#     # Process and display the API response as needed
+#     if api_response:
+#         st.write("API Response:")
+#         st.write(api_response)
+
+#     # Remove the temporary image file after processing
+#     os.remove(image_path)
