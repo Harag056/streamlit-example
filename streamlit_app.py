07f7aadcf92459c622bba7299a0ac27ec54031e1
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-24 17:06:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,127 @@
-import streamlit as st
-import requests
-import json
+import cv2
+import numpy as np
+import tempfile
+import os
 
-# Replace 'YOUR_LANDING_AI_API_KEY' with your actual Landing AI API key
-LANDING_AI_API_KEY = 'land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O'
+def read_video_frames(binary_data, fps):
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(binary_data)
+    temp_file.close()
 
-#LANDING_AI_UPLOAD_URL = 'https://predict.app.landing.ai/inference/v1/predict?endpoint_id=5bc96d69-6328-410f-83e2-eb3b5d97ad29'
-LANDING_AI_UPLOAD_URL = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=5bc96d69-6328-410f-83e2-eb3b5d97ad29"
+    video = cv2.VideoCapture(temp_file.name)
 
-def upload_image_to_landing_ai(image_path):
-    headers = {
-        'apikey': 'land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O'
-    }
-    payload = {}
-    files = {'file': (image_path,open(image_path,'rb'),'image/jpeg')}
-    
-    image_path
-    response = requests.request("POST", LANDING_AI_UPLOAD_URL, headers=headers, data=payload, files=files)
-    
-    print(response.text)
-    #response = requests.post(LANDING_AI_UPLOAD_URL, headers=headers, files=files)
+    # Get frames per second (FPS) of the video
+    original_fps = video.get(cv2.CAP_PROP_FPS)
 
-    return response.json()
+    # Calculate frame interval to get desired FPS
+    frame_interval = int(original_fps / fps)
+
+    frames = []
+    frame_count = 0
+
+    while True:
+        ret, frame = video.read()
+
+        if not ret:
+            break
+
+        if frame_count % frame_interval == 0:
+            frames.append(frame)
+
+        frame_count += 1
+
+    video.release()
+    os.unlink(temp_file.name)
+    return frames
 
 def main():
-    st.title("Upload Image to Landing AI")
+    st.title("Video to Frames Converter")
 
-    # File uploader to choose an image file from the local machine
-    image_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+    # Binary uploader to choose a video file in binary format
+    binary_file = st.file_uploader("Choose a video file in binary format", type=["bin"])
 
-    if image_file is not None:
-        # Save the uploaded image to a temporary file
-        with open('temp_image.jpg', 'wb') as f:
-            f.write(image_file.read())
+    if binary_file is not None:
+        # Get desired frames per second using a slider
+        desired_fps = st.slider("Select frames per second:", min_value=1, max_value=30, value=10)
 
-        st.image(image_file, caption="Uploaded Image", use_column_width=True)
+        # Convert the binary file to frames and store them in an array
+        binary_data = binary_file.getvalue()
+        frames = read_video_frames(binary_data, desired_fps)
 
-        # Upload the image to Landing AI
-        response_data = upload_image_to_landing_ai('temp_image.jpg')
-       
-        # Set the confidence threshold using a slider
-        confidence_threshold = st.slider("Set Confidence Threshold", min_value=0.0, max_value=1.0, value=0.7, step=0.01)
-        filtered_predictions = [ pred for pred in response_data["backbonepredictions"].values() if pred["score"] >= confidence_threshold]
-        #filtered_predictions = [prediction["score"] for prediction in response_data["backbonepredictions"].values() if prediction["score"] >= confidence_threshold]
-
-        filtered_predictions
-
-        if len(filtered_predictions) > 0:
-            st.write("Predictions meeting the confidence threshold:")
-            for pred in filtered_predictions:
-                st.write(f"Label: {pred['labelName']}, Confidence: {pred['score']:.2f}")
-        else:
-            st.write("No predictions meet the confidence threshold.")
-
-        # Process the response from Landing AI and display the result
-        # if 'prediction' in response_data:
-        #     st.write("Prediction results from Landing AI:")
-        #     for prediction in response_data['prediction']:
-        #         st.write(f"Label: {prediction['label']}, Confidence: {prediction['confidence']:.2f}")
+        # Display the frames one by one
+        for i, frame in enumerate(frames):
+            st.image(frame, caption=f"Frame {i+1}", use_column_width=True)
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+# import streamlit as st
+# import requests
+# import json
+
+# # Replace 'YOUR_LANDING_AI_API_KEY' with your actual Landing AI API key
+# LANDING_AI_API_KEY = 'land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O'
+
+# #LANDING_AI_UPLOAD_URL = 'https://predict.app.landing.ai/inference/v1/predict?endpoint_id=5bc96d69-6328-410f-83e2-eb3b5d97ad29'
+# LANDING_AI_UPLOAD_URL = "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=5bc96d69-6328-410f-83e2-eb3b5d97ad29"
+
+# def upload_image_to_landing_ai(image_path):
+#     headers = {
+#         'apikey': 'land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O'
+#     }
+#     payload = {}
+#     files = {'file': (image_path,open(image_path,'rb'),'image/jpeg')}
+    
+#     image_path
+#     response = requests.request("POST", LANDING_AI_UPLOAD_URL, headers=headers, data=payload, files=files)
+    
+#     print(response.text)
+#     #response = requests.post(LANDING_AI_UPLOAD_URL, headers=headers, files=files)
+
+#     return response.json()
+
+# def main():
+#     st.title("Upload Image to Landing AI")
+
+#     # File uploader to choose an image file from the local machine
+#     image_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+
+#     if image_file is not None:
+#         # Save the uploaded image to a temporary file
+#         with open('temp_image.jpg', 'wb') as f:
+#             f.write(image_file.read())
+
+#         st.image(image_file, caption="Uploaded Image", use_column_width=True)
+
+#         # Upload the image to Landing AI
+#         response_data = upload_image_to_landing_ai('temp_image.jpg')
+       
+#         # Set the confidence threshold using a slider
+#         confidence_threshold = st.slider("Set Confidence Threshold", min_value=0.0, max_value=1.0, value=0.7, step=0.01)
+#         filtered_predictions = [ pred for pred in response_data["backbonepredictions"].values() if pred["score"] >= confidence_threshold]
+#         #filtered_predictions = [prediction["score"] for prediction in response_data["backbonepredictions"].values() if prediction["score"] >= confidence_threshold]
+
+#         filtered_predictions
+
+#         if len(filtered_predictions) > 0:
+#             st.write("Predictions meeting the confidence threshold:")
+#             for pred in filtered_predictions:
+#                 st.write(f"Label: {pred['labelName']}, Confidence: {pred['score']:.2f}")
+#         else:
+#             st.write("No predictions meet the confidence threshold.")
+
+#         # Process the response from Landing AI and display the result
+#         # if 'prediction' in response_data:
+#         #     st.write("Prediction results from Landing AI:")
+#         #     for prediction in response_data['prediction']:
+#         #         st.write(f"Label: {prediction['label']}, Confidence: {prediction['confidence']:.2f}")
+
+# if __name__ == "__main__":
+#     main()
