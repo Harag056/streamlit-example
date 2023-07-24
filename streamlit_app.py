35d08e53befa7dd02419c1aed9099f5df621474b
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-24 11:32:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,82 @@
-import os
-
-# Set the environment variable to prevent graphical backend usage in OpenCV
-os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = str(2**64)
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "protocol_whitelist;file,rtp,udp"
-
-# Import the other necessary libraries after setting the environment variables
-
 import streamlit as st
-import numpy as np
-from PIL import Image
-#import cv2 
 
-def convert_video_to_binary(video_file):
-    binary_data = video_file.read()
-    return binary_data
+def home():
+    st.title("Welcome to the Home Page!")
+    st.write("This is the home page content.")
 
-def get_video_frames(binary_data):
-    # Convert binary data to numpy array
-    np_array = np.frombuffer(binary_data, dtype=np.uint8)
+def about():
+    st.title("About Us")
+    st.write("This is the About Us page content.")
 
-    # Read the video from the numpy array
-    video = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
-
-    # Get frames per second (FPS) of the video
-    fps = video.get(cv2.CAP_PROP_FPS)
-
-    # Extract one frame per second
-    frame_interval = int(fps)  # Retrieve one frame per second
-    frames = [Image.fromarray(frame) for i, frame in enumerate(video) if i % frame_interval == 0]
-
-    return frames
+def contact():
+    st.title("Contact Us")
+    st.write("This is the Contact Us page content.")
 
 def main():
-    st.title("Video to Frames Converter")
+    st.title("Streamlit App with Menu Bars")
 
-    # File uploader to choose a video file from the local machine
-    video_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mkv"])
+    # Create a sidebar with menu options
+    menu = st.sidebar.selectbox("Select an option:", ("Home", "About", "Contact"))
 
-    if video_file is not None:
-        # Convert the video to binary data
-        binary_data = convert_video_to_binary(video_file)
-
-        # Get frames from the video
-        frames = get_video_frames(binary_data)
-
-        # Display the frames
-        for i, frame in enumerate(frames):
-            st.image(frame, caption=f"Frame {i+1}", use_column_width=True)
+    if menu == "Home":
+        home()
+    elif menu == "About":
+        about()
+    elif menu == "Contact":
+        contact()
 
 if __name__ == "__main__":
     main()
+
+# import os
+
+# # Set the environment variable to prevent graphical backend usage in OpenCV
+# os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = str(2**64)
+# os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "protocol_whitelist;file,rtp,udp"
+
+# # Import the other necessary libraries after setting the environment variables
+
+# import streamlit as st
+# import numpy as np
+# from PIL import Image
+# #import cv2 
+
+# def convert_video_to_binary(video_file):
+#     binary_data = video_file.read()
+#     return binary_data
+
+# def get_video_frames(binary_data):
+#     # Convert binary data to numpy array
+#     np_array = np.frombuffer(binary_data, dtype=np.uint8)
+
+#     # Read the video from the numpy array
+#     video = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
+
+#     # Get frames per second (FPS) of the video
+#     fps = video.get(cv2.CAP_PROP_FPS)
+
+#     # Extract one frame per second
+#     frame_interval = int(fps)  # Retrieve one frame per second
+#     frames = [Image.fromarray(frame) for i, frame in enumerate(video) if i % frame_interval == 0]
+
+#     return frames
+
+# def main():
+#     st.title("Video to Frames Converter")
+
+#     # File uploader to choose a video file from the local machine
+#     video_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mkv"])
+
+#     if video_file is not None:
+#         # Convert the video to binary data
+#         binary_data = convert_video_to_binary(video_file)
+
+#         # Get frames from the video
+#         frames = get_video_frames(binary_data)
+
+#         # Display the frames
+#         for i, frame in enumerate(frames):
+#             st.image(frame, caption=f"Frame {i+1}", use_column_width=True)
+
+# if __name__ == "__main__":
+#     main()
