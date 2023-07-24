c5c41f0bf89ef4fc2811de136685201686424a20
Harish Sankaranarayanan <105298836+Harag056@users.noreply.github.com>
2023-07-24 13:40:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import os
 
 def Connection():
     st.title("Connection App")
-
+    st.write("Snowflake Connection Details")
     #Input fields for Snowflake connection parameters
     snowflake_account = st.text_input("Snowflake Account", "rz20203.central-india.azure")
     snowflake_username = st.text_input("User", "TESTUSER")
@@ -18,7 +18,9 @@ def Connection():
     snowflake_warehouse = st.text_input("Warehouse", "COMPUTE_WH")
     snowflake_database = st.text_input("Database", "LandingAI_DB")
     snowflake_schema = st.text_input("Schema", "Raw")
-
+    st.write("Snowflake Connection Details")
+    landingai_api_key = st.text_input("Api Key", "land_sk_0EJDSLM53NDshwkFBKbuYzIKv2g7oaUeQ1zXLhBC2AeQKXLj0O", type="password")
+    landinai_endpoint_id = st.text_input("EndpointId", "5bc96d69-6328-410f-83e2-eb3b5d97ad29")
     # Function to connect to Snowflake
     def connect_to_snowflake():
         conn = snowflake.connector.connect(
